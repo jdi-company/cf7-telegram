68330af5fc0443f8d68667c3ae2d104d2c190e9d
Yaroslav Borodii <borodij.yaroslav@gmail.com>
2025-08-07 22:59:30 +0300

    Update to v0.10.0

--- plugin-dir/cf7-telegram.php
+++ plugin-dir/cf7-telegram.php
@@ -3,19 +3,22 @@
 * Plugin Name: Contact Form 7 + Telegram
 * Description: Sends messages to Telegram-chat
 * Author: Hokku
-* Version: 0.9.3
+* Version: 0.10.0
 * License: GPL v2 or later
 * License URI: https://www.gnu.org/licenses/gpl-2.0.html
 * Text Domain: cf7-telegram
 * Domain Path: /languages
 */
 
+use YahnisElsts\PluginUpdateChecker\v5\PucFactory;
+
 define( 'WPCF7TG_PLUGIN_NAME', plugin_basename( __FILE__ ) );
 
-define( 'WPCF7TG_VERSION', '0.9.3' );
+define( 'WPCF7TG_VERSION', '0.10.0' );
 define( 'WPCF7TG_FILE', __FILE__ );
 
 const WPCF7TG_MIGRATION_HOOK = 'cf7tg_migrations';
+require __DIR__ . '/vendor/autoload.php';
 
 require ( __DIR__ . '/classes/wpcf7telegram.php' );
 wpcf7_Telegram::get_instance();
@@ -70,3 +73,16 @@ add_action(
 	10, 2
 );
 
+if ( wpcf7_Telegram::get_instance()->pre_releases ) {
+	$updateChecker = PucFactory::buildUpdateChecker(
+		'https://github.com/hokoo/cf7-telegram',
+		WPCF7TG_FILE,
+		'cf7-telegram'
+	);
+
+	if ( defined( 'WPCF7TG_GITHUB_TOKEN' ) ) {
+		$updateChecker->setAuthentication( WPCF7TG_GITHUB_TOKEN );
+	}
+
+	$updateChecker->setBranch( 'plugin-dist' );
+}
